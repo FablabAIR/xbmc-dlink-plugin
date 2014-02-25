51d79b059e2860c4cb17fb4f5b457f27c1f98935
PierreLartigue <lartigue.pierre@gmail.com>
2014-02-25 09:55:34 +0100

    icons added. release ?!

--- addon.py
+++ addon.py
@@ -45,7 +45,7 @@ class Camera:
 	  HOST: %s:%s
 	  MAN: ssdp:discover
 	  MX: 5
-	  ST: upnp:rootdevice""" % (ip, port)
+	  ST: upnp:rootservice""" % (ip, port)
 	  #ST : ssdp:all"""
 	  client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -61,13 +61,48 @@ class Camera:
 		  notify("Camera discovery", recv_data )
 		  
 	  return camera_ip
-	  
+
+
+	
+class buttonWindow(xbmcgui.WindowXMLDialog):
+		def __init__(self, strXMLname, strFallBacklPath, strDefaultName, forceFallback):
+			pass
+	
+
+class streamPlayer(xbmc.Player):
+	def __init__(self):
+		xbmc.Player.__init__(xbmc.PLAYER_CORE_MPLAYER)
+		self.win = None
+		
+	def setWindow(self, window):
+		self.win = window	
+	
+	def onPlaybackStopped(self):
+		if not self.win == 'None' :
+			self.win.close()
+			del self.win
+
+		
+	
 def notify(title, content):
 	xbmc.executebuiltin('Notification(%s, %s)' %(title, content))
   
   
 def runvideo(camera):
-	xbmc.Player().play('rtsp://'+str(camera.camera_ip)+'/play1.sdp')
+	win = buttonWindow("playerButtons.xml",camera.addon.getAddonInfo('path'),"Default","720p")
+	player = streamPlayer()
+	player.setWindow(win)
+
+	player.play('rtsp://'+str(camera.camera_ip)+'/play1.sdp')
+	
+	win.doModal()
+	del win
+
+	#xbmc.Player().play('rtsp://'+str(camera.camera_ip)+'/play1.sdp')
+	#xbmc.Player().play('http://192.168.0.21/video.cgi')
+	
+
+	
 
 if ( __name__ == "__main__" ):
 	settings = settings()
@@ -101,4 +136,17 @@ if ( __name__ == "__main__" ):
 			cam.right()
 		elif sys.argv[1] == 'home':
 			cam.home()
+		elif sys.argv[1] == 'upleft':
+			cam.upleft()
+		elif sys.argv[1] == 'downleft':
+			cam.downleft()
+		elif sys.argv[1] == 'downright':
+			cam.downright()
+		elif sys.argv[1] == 'upright':
+			cam.upright()
+		
+			
+			
+			
+		
 	
